b7827cbf6d93386ff98c6df0f948a4478b1c368a
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-11-25 11:59:25 +0530

    Source Code for Hands on Ex.5 for Module Three

---
+++ ModThreeEXFive.java
@@ -0,0 +1,13 @@
+// finding odd numbers in 100 integers
+public class ModThreeEXFive {
+    public static void main(String[] args){
+
+        for(int i=1;i<=100;i++){
+
+            if(i%2 !=0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
