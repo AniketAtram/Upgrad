6eb36d117a3e111febb2368020c28ce210e1ea31
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2021-11-25 11:58:32 +0530

    Source Code for Hands on Ex.2 for Module Three

---
+++ ModThreeEXTwo.java
@@ -0,0 +1,11 @@
+public class ModThreeEXTwo {
+    public static void main(String[] args){
+
+        int counter = 1;
+
+        do{
+            System.out.println(counter *2);
+            counter ++;
+        }while(counter<=10);
+    }
+}
